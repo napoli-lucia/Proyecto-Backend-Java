d43c582f832f3bf4b9a966d41e737b8676ce37f9
Lucia Napoli <lucianapoli15@gmail.com>
2023-08-21 17:43:05 -0300

    creo proyecto con springboot

---
+++ events-manager/src/main/java/metcamp/backend/eventsmanager/EventsManagerApplication.java
@@ -0,0 +1,13 @@
+package metcamp.backend.eventsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsManagerApplication.class, args);
+	}
+
+}
---
+++ events-manager/src/test/java/metcamp/backend/eventsmanager/EventsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package metcamp.backend.eventsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
